6daf74937c9255a79fc6e5dfec39353b926347ca
Evilgeniusnerd <kd2ael@gmail.com>
2020-05-16 10:40:05 -0400

    Fixed Heltec & TTgo Code
    Signed-off-by: Evilgeniusnerd <kd2ael@gmail.com>

--- examples/BasicSend_v2_Heltec_Lora/BasicSend_v2_Heltec_Lora.ino
+++ examples/BasicSend_v2_Heltec_Lora/BasicSend_v2_Heltec_Lora.ino
@@ -2,12 +2,6 @@
 
 /*
    BasicSend
-
-   This sketch sends a "Hello, world!" message from the satellite modem.
-   If you have activated your account and have credits, this message
-   should arrive at the endpoints you have configured (email address or
-   HTTP POST).
-
    you can't use U0TXD and U0RXD becuase they are used for the programming of the board, quite why Heltec brought these pins
    out to the edge connector I don't know, I suppose to allow an external programmer, who knows.
    The pins that are free are as follows:
@@ -32,40 +26,93 @@
 
 #define IridiumSerial Serial2
 #define DIAGNOSTICS false// Change this to see diagnostics
-#define RXD2 12
-#define TXD2 13
+#define rxpin 12
+#define txpin 13
 
 // Declare the IridiumSBD object
 IridiumSBD modem(IridiumSerial);
 
-
 void setup()
 {
-  Serial.begin(19200);
-  Serial.println("Starting setup...");
-
   int signalQuality = -1;
-        int err;
+  int err;
+
+  // Start the console serial port
+  Serial.begin(1500);
+  while (!Serial);
 
   // Start the serial port connected to the satellite modem
-  Serial.println("Starting satellite serial connection (swSer)");
-  Serial2.begin(19200, SERIAL_8N1, RXD2, TXD2);
+  Serial2.begin(9600, SERIAL_8N1, rxpin, txpin, false); // false means normal data polarity so steady state of line = 0, true means staedy state = high.
 
   // Begin satellite modem operation
   Serial.println("Starting modem...");
   err = modem.begin();
-  Serial.print("modem: ");
-  Serial.println(err);
-
   if (err != ISBD_SUCCESS)
   {
-    Serial.print("Begin failed: error ");
+    Serial.print("Modem begin failed: error ");
     Serial.println(err);
     if (err == ISBD_NO_MODEM_DETECTED)
       Serial.println("No modem detected: check wiring.");
     return;
   }
+
+  // Example: Print the firmware revision
+  char version[12];
+  err = modem.getFirmwareVersion(version, sizeof(version));
+  if (err != ISBD_SUCCESS)
+  {
+    Serial.print("FirmwareVersion failed: error ");
+    Serial.println(err);
+    return;
+  }
+  Serial.print("Firmware Version is ");
+  Serial.print(version);
+  Serial.println(".");
+
+  // Example: Test the signal quality.
+  // This returns a number between 0 and 5.
+  // 2 or better is preferred.
+  err = modem.getSignalQuality(signalQuality);
+  if (err != ISBD_SUCCESS)
+  {
+    Serial.print("SignalQuality failed: error ");
+    Serial.println(err);
+    return;
+  }
+
+  Serial.print("On a scale of 0 to 5, signal quality is currently ");
+  Serial.print(signalQuality);
+  Serial.println(".");
+
+  // Send the message
+  Serial.print("Trying to send the message.  This might take several minutes.\r\n");
+  err = modem.sendSBDText("Hello, world!");
+  if (err != ISBD_SUCCESS)
+  {
+    Serial.print("sendSBDText failed: error ");
+    Serial.println(err);
+    if (err == ISBD_SENDRECEIVE_TIMEOUT)
+      Serial.println("Try again with a better view of the sky.");
+  }
+
+  else
+  {
+    Serial.println("Hey, it worked!");
+  }
 }
 
-void loop() {
+void loop()
+{
 }
+
+#if DIAGNOSTICS
+void ISBDConsoleCallback(IridiumSBD *device, char c)
+{
+  Serial.write(c);
+}
+
+void ISBDDiagsCallback(IridiumSBD *device, char c)
+{
+  Serial.write(c);
+}
+#endif
